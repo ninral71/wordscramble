4532af5084c3b61e85dc7004798be3704b60000a
ninral71 <115190936+ninral71@users.noreply.github.com>
2022-10-06 17:19:55 -0400

    Add files via upload

---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "Security",
+        hint: "Keeping people/data safe from threats"
+    },
+    {
+        word: "Password",
+        hint: "Phrase only you know"
+    },
+    {
+        word: "October",
+        hint: "National Cyber Security Awareness Month"
+    },
+    {
+        word: "Malware",
+        hint: "Malicious software"
+    },
+    {
+        word: "Phishing",
+        hint: "Attempt, typically through email, to gather personal information from you"
+    },
+    {
+        word: "Interactions",
+        hint: "The company we work for"
+    },
+    {
+        word: "Computer",
+        hint: "The device you are most likely currently on"
+    },
+    {
+        word: "Virus",
+        hint: "Code capable of replicating itself"
+    },
+    {
+        word: "Ransomware",
+        hint: "A type of attack where the attacks demands money in exchange for resources they stole"
+    },
+    {
+        word: "Vulnerability",
+        hint: "Weakness that can be exploited"
+    },
+    {
+        word: "Data",
+        hint: "Resources that security aims to protect"
+    },
+    {
+        word: "Hardware",
+        hint: "Computers tangible components"
+    },
+    {
+        word: "Software",
+        hint: "Computers intangible components"
+    },
+    {
+        word: "Firewall",
+        hint: "Defensive technology designed to keep bad guys out"
+    },
+    {
+        word: "Network",
+        hint: "System of connected computers"
+    },
+    {
+        word: "Encryption",
+        hint: "Process of encoding data with a secret key"
+    },
+    {
+        word: "Decryption",
+        hint: "Process of decoding data with a secret key"
+    },
+    {
+        word: "Authentication",
+        hint: "Process of verifying the identity of a user"
+    },
+    {
+        word: "Confidentiality",
+        hint: "Protecting sensitive information"
+    },
+    {
+        word: "Wifi",
+        hint: "Wireless internet connection"
+    },
+    {
+        word: "Keyboard",
+        hint: "Thing you use to type"
+    },
+    {
+        word: "Mouse",
+        hint: "Moves cursor on screen, also an animal"
+    },
+    {
+        word: "Cybercrime",
+        hint: "Crimes committed using a computer/Internet"
+    },
+]
